a53193f10c6eb67b88c0314001efcc9897ae0706
Serafym Starobrianskyi <starobranskijs@gmail.com>
2026-04-16 22:55:56 +0200

    feat: add sidebar, homepage, folderpage, settingpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/homePage/HomePage"
+import ProjectsPage from "./pages/projectsPage/ProjectsPage"
+import SettingsPage from "./pages/settingsPage/SettingsPage"
 
 function App() {
-  return (<></>)
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/projects" element={<ProjectsPage/>}/>
+      <Route path="/settings" element={<SettingsPage/>}/>
+    </Routes>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/settingsPage/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout/Layout";
+import "./settings-page.css";
+
+export default function SettingsPage(){
+  return (
+    <Layout>
+      <section className="settings-page">
+        <h1 className="settings-page__title">Settings</h1>
+      </section>
+    </Layout>
+  )
+}
